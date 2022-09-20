5c6cd8dd65f4d24b0bc39132b744e476de744f93
Tr4shL0rd <ketil04@gmail.com>
2022-09-20 19:47:09 +0200

    added testing for Player.beats()

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -6,5 +6,21 @@ from rps.player import Player
 
 import pytest
 
-def test_beats_valid():
+def test_beats_valid_card_rock():
     assert Player.beats("rock", "scissors") == True
+
+def test_beats_valid_card_scissors():
+    assert Player.beats("scissors", "paper") == True
+
+def test_beats_valid_card_paper():
+    assert Player.beats("paper", "rock") == True
+
+def test_beats_valid_card_draw():
+    assert Player.beats("paper", "paper") == False
+
+
+def test_beats_invalid():
+    expected_exception = KeyError
+    with pytest.raises(expected_exception=expected_exception) as excInfo:
+        Player.beats("not a rock", "rock")
+    excInfo.value == expected_exception

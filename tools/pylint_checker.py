88714eb182b23b023efbd8438b3d4d4d95856a27
Tr4shL0rd <ketil04@gmail.com>
2022-09-23 10:30:44 +0200

    rename and formatting

---
+++ tools/pylint_checker.py
@@ -0,0 +1,17 @@
+from pylint.lint import Run
+from pylint.reporters.text import TextReporter
+
+with open("tools/reports/pylintReport.txt", "w") as f:
+    reporter = TextReporter(f)
+    tracked_files = [
+                        "rps/game.py", "rps/player.py",
+                        "tests/manual_testing.py","tests/test_beats.py",
+                        "tests/test_cards.py","tests/test_stats.py",
+                        "tools/check_reqs.py","tools/pylintChecker.py"
+                    ]
+
+    Run(tracked_files, reporter=reporter, exit=False)
+with open("tools/reports/pylintReport.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line.replace("\n", ""))

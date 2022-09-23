1d1e27854010c30147d912d93db60d4bbc0a228e
Tr4shL0rd <ketil04@gmail.com>
2022-09-23 09:29:11 +0200

    formatting

--- rps/game.py
+++ rps/game.py
@@ -1,88 +1,119 @@
+"""
+    player module keeps track of game stats and player stats
+"""
+from random import choice
+import sys
 from player import Player
 from player import CardClass
 from rich import print as rprint
-from random import choice
-
 class Game:
+    """
+        tracks player, game and opponent.
+    """
     def __init__(self) -> None:
         pass
-    player   = Player
+    player = Player
     opponent = Player
     playerStats = player.Stats(
-                name="Hero",
-                score=0,
-                cards=CardClass.Cards()
-                )
+        name="Hero",
+        score=0,
+        cards=CardClass.cards()
+    )
     opponentStats = opponent.Stats(
-                    name="Opponent",
-                    score=0,
-                    cards=CardClass.Cards()
-                    )
+        name="Opponent",
+        score=0,
+        cards=CardClass.cards()
+    )
     gameDraws = Player.Stats(
-                name="TIE",
-                score=0,
-                cards=CardClass.Cards()
-                )
+        name="TIE",
+        score=0,
+        cards=CardClass.cards()
+    )
     gameRounds = Player.Stats(
-                name="ROUNDS",
-                score=1,
-                cards=CardClass.Cards()
-                )
-    def opponentCard(self):
+        name="ROUNDS",
+        score=1,
+        cards=CardClass.cards()
+    )
+    """
+        :return str: a string containing the card the opponent uses
+    """
+    def opponent_card(self) -> str:
+        """
+            Game.OpponentStats.cards is a list
+        """
         return choice(Game.opponentStats.cards)
-    def newPlayerCard(self):
-        return
-    def draw(self) -> tuple[bool,str]:
-        Cards = CardClass
-        cardStack = self.playerStats.cards
-        prettyCards = ", ".join(cardStack)
-        rprint(f"Your Cards: {', '.join(Cards.ColorCards(prettyCards))}")
-        drawCard = str(input("Draw A Card: ")).lower()
-        if drawCard not in self.playerStats.cards:
-            print(f"{drawCard} is not in your deck!")
-            self.draw()
-        return (Player.beats(drawCard, self.opponentCard()), drawCard)
 
-def gameLoop(running=True):
+    def draw(self) -> tuple[bool, str]:
+        """
+        :return tuple[bool,str]:
+        :bool: boolean if the player card beats the opponent card
+        :str:  the card the player drew
+        """
+        Cards = CardClass
+        card_stack = self.playerStats.cards
+        pretty_cards = ", ".join(card_stack)
+        rprint(f"Your Cards: {', '.join(Cards.color_cards(pretty_cards))}")
+        draw_card = str(input("Draw A Card: ")).lower()
+        if draw_card not in self.playerStats.cards:
+            print(f"{draw_card} is not in your deck!")
+            self.draw()
+        return (Player.beats(card1=draw_card, card2=self.opponent_card()), draw_card)
+
+def game_loop(running=True):
+    """
+        :param running bool: a boolean value dictation if the game is runnig
+        :returns:
+    """
     game = Game()
     while running:
         if game.gameRounds.score == 1:
-            rprint(f"[blue]{Game.playerStats.name}[/blue] VS [red]{Game.opponentStats.name}[/red]")
+            rprint(f"""
+                    [blue]{Game.playerStats.name}[/blue] VS [red]{Game.opponentStats.name}[/red]
+                    """)
         if game.gameRounds.score >= 10:
-            rprint(f"""[blue underline]{game.playerStats.name}'s[/blue underline] Score: {game.playerStats.score}""")
-            rprint(f"[red underline]{game.opponentStats.name}'s[/red underline] Score: {game.opponentStats.score}")
-            rprint(f"{game.gameDraws.score} [yellow underline]{game.gameDraws.name}(S)[/yellow underline]")
+            rprint(f"""
+                    [blue underline]{game.playerStats.name}'s[/blue underline]"
+                    Score: {game.playerStats.score}
+                    """)
+            rprint(f"""
+                [red underline]{game.opponentStats.name}'s[/red underline] Score: {game.opponentStats.score}
+                """)
+            rprint(f"""
+                    {game.gameDraws.score} [yellow underline]{game.gameDraws.name}(S)[/yellow underline]
+                """)
             if game.playerStats.score == 10:
                 print("YOU WIN THE GAME!")
             elif game.opponentStats.score == 10:
                 print("YOU LOSE THE GAME!")
-            exit()
+            sys.exit()
         else:
             print(game.gameRounds.score)
             rprint(f"Your Score: {game.playerStats.score}")
-        cpuCard = game.opponentCard()
-        cardDownBeat,cardDrawn = game.draw()
-        if cardDrawn == cpuCard:
+        cpu_card = game.opponent_card()
+        card_down_beat, card_drawn = game.draw()
+        if card_drawn == cpu_card:
             rprint("[yellow underline]TIE[/yellow underline]")
             game.gameDraws.score += 1
-            
-        if cardDownBeat:
+
+        if card_down_beat:
             game.playerStats.score += 1
             rprint("[green]WIN[/green]")
-            rprint(f"[blue]{cardDrawn}[/blue] beats [red]{cpuCard}[/red]")
+            rprint(f"[blue]{card_drawn}[/blue] beats [red]{cpu_card}[/red]")
             print("You Win This Round!")
-        elif not cardDownBeat and not cardDrawn == cpuCard:
+        elif not card_down_beat and not card_drawn == cpu_card:
             game.opponentStats.score += 1
             rprint("[red]LOSS[/red]")
-            rprint(f"[red]{cpuCard}[/red] beats [blue]{cardDrawn}[/blue]")
+            rprint(f"[red]{cpu_card}[/red] beats [blue]{card_drawn}[/blue]")
             print("You Lost This Round!")
-        if cardDownBeat or not cardDownBeat:
+        if card_down_beat or not card_down_beat:
             game.gameRounds.score += 1
 
         print("")
-        game.playerStats.cards = CardClass.Cards()
+        game.playerStats.cards = CardClass.cards()
+
+
 try:
     while True:
-        gameLoop()
+        game_loop()
 except KeyboardInterrupt:
     print("\nExiting...")
--- rps/player.py
+++ rps/player.py
@@ -1,9 +1,12 @@
+"""
+    This module keeps track of player stats and card decks
+"""
 from dataclasses import astuple, dataclass
-
-
-from enforce_typing import enforce_types
 from random import choice
 from typing import Iterator
+from enforce_typing import enforce_types
+
+
 
 
 class Player:
@@ -12,17 +15,18 @@ class Player:
     '''
     def __init__(self) -> None:
         pass
-    def beats(card1:str,card2:str) -> bool:
+    @classmethod
+    def beats(cls,card1:str,card2:str) -> bool:
         """
         :param card1 str: Players card\n
         :param card2 str: Opponents  card\n
         :returns bool: True if card1 beats card2
-        :raises KeyError: KeyError is raised when one of the cards are invalid 
+        :raises KeyError: KeyError is raised when one of the cards are invalid
         """
-        card1,card2 = card1.lower(),card2.lower() 
-        validCards = ["rock", "paper", "scissors"]
-        if card1 not in validCards or card2 not in validCards:
-            raise KeyError(f"card1 or card2 is not a valid card!")
+        card1,card2 = card1.lower(),card2.lower()
+        valid_cards = ["rock", "paper", "scissors"]
+        if card1 not in valid_cards or card2 not in valid_cards:
+            raise KeyError("card1 or card2 is not a valid card!")
         match (card1,card2):
             case ("rock","scissors"):
                 return True
@@ -32,6 +36,11 @@ class Player:
                 return True
             case _:
                 return False
+    def check_player_card(self,player_card:str) -> bool:
+        """
+            checks if player_card is a string
+        """
+        return isinstance(player_card,str)
 
     @enforce_types
     @dataclass
@@ -47,12 +56,16 @@ class Player:
                 enables iteration over each stat in the Player class
             '''
             return iter(astuple(self))
-        
-    
+
+
 class CardClass:
+    """
+        class the represents a stack of cards
+    """
     def __init__(self) -> None:
         pass
-    def Cards() -> list[str,str,str]:
+    @classmethod
+    def cards(cls) -> list[str,str,str]:
         """
         :returns list: the list contains 3 cards of type str"
         """
@@ -61,13 +74,17 @@ class CardClass:
                 choice(["rock", "paper", "scissors"]),
                 choice(["rock", "paper", "scissors"])
                 ]
-    def ColorCards(cards:str):
+    @classmethod
+    def color_cards(cls,cards:str) -> list:
+        """
+            :returns list: containing pretty displayable cards
+        """
         colors = {
             "rock":     "[grey37]rock[/grey37]",
             "paper":    "[bright_white]paper[/bright_white]",
             "scissors": "[bright_magenta]scissors[/bright_magenta]"
         }
-        prettyCards = []
+        pretty_cards = []
         for card in cards.split(", "):
-            prettyCards.append(colors[card])
-        return prettyCards
+            pretty_cards.append(colors[card])
+        return pretty_cards

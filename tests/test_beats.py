1ef3bf5a1bafd05088458cffadab39118db36829
Tr4shL0rd <ketil04@gmail.com>
2022-09-23 12:08:57 +0200

    black formatting

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -5,41 +5,40 @@ import pytest
 from rps.player import Player
 
 
-
 def test_beats_valid_card_rock():
     """
-        test for rock
+    test for rock
     """
     assert Player.beats("rock", "scissors") is True
 
 
 def test_beats_valid_card_scissors():
     """
-        test for scissors
+    test for scissors
     """
     assert Player.beats("scissors", "paper") is True
 
 
 def test_beats_valid_card_paper():
     """
-        test for paper
+    test for paper
     """
     assert Player.beats("paper", "rock") is True
 
 
 def test_beats_valid_card_draw():
     """
-        test for same draw
+    test for same draw
     """
     assert Player.beats("paper", "paper") is False
 
 
 def test_beats_invalid():
     """
-        test for invalid card
+    test for invalid card
     """
 
     expected_exception = KeyError
     with pytest.raises(expected_exception=expected_exception) as exc_info:
         Player.beats("not a rock", "rock")
-    exc_info.value == expected_exception # pylint: disable=pointless-statement
+    exc_info.value == expected_exception  # pylint: disable=pointless-statement
